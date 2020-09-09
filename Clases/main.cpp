1dc6e641b3c6bb9c8d79afc4ddc41049b6c8f6f3
Daniel Alfredo Hernandez Grimaldi <dannyhgrimaldi@gmail.com>
2020-09-09 08:57:05 -0500

    Initial Commit

---
+++ Clases/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Clases
+//
+//  Created by Daniel Hernández Grimaldi on 09/09/20.
+//  Copyright © 2020 Daniel Hernández Grimaldi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
